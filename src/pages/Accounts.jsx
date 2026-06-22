07765fdc5a5863bfbc15575505dc7aa1dd763bb9
abhishek9874-ctrl <sanjeetsinghrbs@gmail.com>
2026-06-22 23:06:31 +0530

    Completed accounts page and account cards

--- src/pages/Accounts.jsx
+++ src/pages/Accounts.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import SideBar from '../components/SideBar'
 import Header from '../components/Header'
+import axios from "axios";
 import {
     Landmark,
     Wallet,
@@ -9,6 +10,57 @@ import {
 } from "lucide-react";
 
 function Accounts() {
+    const [accounts, setAccounts] = useState([]);
+    const [totalBalance, setTotalBalance] = useState(0);
+
+    useEffect(() => {
+        const fetchAccounts = async () => {
+            try {
+                const token = localStorage.getItem("token");
+
+                const response = await axios.get(
+                    `http://localhost:3000/api/account`,
+                    {
+                        headers: {
+                            Authorization: `Bearer ${token}`,
+                        },
+                    }
+                );
+
+                setAccounts(response.data.accounts);
+
+
+                setAccounts(response.data.accounts);
+
+                if (response.data.accounts.length > 0) {
+
+                    const accountId = response.data.accounts[0]._id;
+
+                    const balanceResponse = await axios.get(
+                        `http://localhost:3000/api/account/balance/${accountId}`,
+                        {
+                            headers: {
+                                Authorization: `Bearer ${token}`,
+                            },
+                        }
+                    );
+
+                    setTotalBalance(balanceResponse.data.balance);
+                }
+
+
+            } catch (err) {
+                console.log(err.response?.data)
+            }
+        };
+        fetchAccounts();
+    }, []);
+
+
+
+
+
+
     return (
         <div className='flex flex-col md:flex-row min-h-screen bg-gray-50'>
             <SideBar />
@@ -16,22 +68,22 @@ function Accounts() {
             <div className='flex-1 bg-gray-50'>
                 <Header />
 
-                <h1 className='md:text-3xl text-xl font-semibold md:px-10 px-2 md:py-10 md:mt-5 mt-10 mb-10 md:mb-5'>Manage All Your accounts here...!!!</h1>
-                <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-6">
+                <h1 className='md:text-3xl text-xl font-semibold md:px-10 px-2 md:py-10 md:mt-5 mt-10 mb-10 md:mb-5'>Manage all your accounts in one place.</h1>
+                <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
 
                     {/* Total Accounts */}
-                    <div className="bg-white border border-gray-200 rounded-3xl px-8 py-7 flex items-center gap-5 hover:shadow-md transition">
+                    <div className="bg-white border md:h-40 border-gray-200 rounded-3xl px-8 py-7 flex items-center gap-5 hover:shadow-md transition">
                         <div className="w-20 h-20 rounded-2xl bg-blue-100 flex items-center justify-center">
-                            <Landmark size={40} className="text-blue-600" />
+                            <Landmark size={50} className="text-blue-600" />
                         </div>
 
                         <div>
-                            <p className="text-gray-500">
+                            <p className="text-gray-500 text-2xl">
                                 Total Accounts
                             </p>
 
                             <h2 className="text-3xl font-bold mt-1">
-                                3
+                                {accounts.length}
                             </h2>
                         </div>
                     </div>
@@ -39,16 +91,17 @@ function Accounts() {
                     {/* Total Balance */}
                     <div className="bg-white border border-gray-200 rounded-3xl px-8 py-8 min-h-[140px] flex items-center gap-6 hover:shadow-md transition">
                         <div className="w-20 h-20 rounded-2xl bg-green-100 flex items-center justify-center">
-                            <Wallet size={40} className="text-green-600" />
+                            <Wallet size={50} className="text-green-600" />
                         </div>
 
                         <div>
-                            <p className="text-gray-500">
+                            <p className="text-gray-500 text-2xl">
                                 Total Balance
                             </p>
 
-                            <h2 className="text-3xl font-bold text-green-600 mt-1">
-                                ₹1,57,000
+
+                            <h2 className="text-2xl font-bold text-black mt-1">
+                                ₹{totalBalance.toLocaleString("en-IN")}
                             </h2>
                         </div>
                     </div>
@@ -56,36 +109,113 @@ function Accounts() {
                     {/* Active Accounts */}
                     <div className="bg-white border border-gray-200 rounded-3xl px-8 py-7 flex items-center gap-5 hover:shadow-md transition">
                         <div className="w-20 h-20 rounded-2xl bg-purple-100 flex items-center justify-center">
-                            <BadgeCheck size={40} className="text-purple-600" />
+                            <BadgeCheck size={50} className="text-purple-600" />
                         </div>
 
                         <div>
-                            <p className="text-gray-500">
+                            <p className="text-gray-500 text-2xl">
                                 Active Accounts
                             </p>
 
                             <h2 className="text-3xl font-bold text-purple-600 mt-1">
-                                3
+                                {accounts.filter(
+                                    account => account.status === 'ACTIVE'
+                                ).length}
                             </h2>
                         </div>
                     </div>
 
                     {/* Inactive Accounts */}
-                    <div className="bg-white border border-gray-200 rounded-3xl px-8 py-7 flex items-center gap-5 hover:shadow-md transition">
-                        <div className="w-20 h-20 rounded-2xl bg-orange-100 flex items-center justify-center">
-                            <Clock3 size={40} className="text-orange-500" />
+
+
+                </div>
+                {/* My Accounts */}
+                <div className="mt-12">
+
+                    <h2 className="text-3xl font-bold mb-6">
+                        My Accounts
+                    </h2>
+
+                    {/* Account Card */}
+                    {accounts.map((account)=>(
+                    <div key={account._id} className="bg-white border border-gray-200 rounded-3xl px-6 py-8 hover:shadow-lg transition-all duration-300">
+
+                        <div className="flex flex-col xl:flex-row xl:items-center xl:justify-between md:gap-6 gap-10">
+
+                            {/* Left Section */}
+                            <div className="flex items-center md:gap-6 gap-10">
+
+                                <div className="w-20 h-20 rounded-3xl bg-blue-100 flex items-center justify-center">
+                                    <Landmark size={40} className="text-blue-600" />
+                                </div>
+
+                                <div>
+                                    <h3 className="text-2xl font-semibold">
+                                        Account
+                                    </h3>
+
+                                    <p className="text-gray-500 mt-2">
+                                        Account ID
+                                    </p>
+
+                                    <p className="text-xl">
+                                        {account._id}
+                                    </p>
+                                </div>
+
+                            </div>
+
+                            {/* Middle Section */}
+                            <div className="grid grid-cols-2 xl:flex xl:flex-wrap gap-6 xl:gap-12">
+
+                                <div>
+                                    <p className="text-gray-500">
+                                        Currency
+                                    </p>
+
+                                    <h4 className="text-xl font-medium mt-1">
+                                        {account.currency}
+                                    </h4>
+                                </div>
+
+                                <div>
+                                    <p className="text-gray-500">
+                                        Created On
+                                    </p>
+
+                                    <h4 className="text-xl font-medium mt-1">
+                                        {new Date(account.createdAt).toLocaleDateString("en-IN")}
+                                    </h4>
+                                </div>
+
+                                <div>
+                                    <p className="text-gray-500 ">
+                                        Balance
+                                    </p>
+
+                                    <h4 className="text-3xl font-bold mt-1">
+                                        ₹{totalBalance.toLocaleString("en-IN")}
+                                    </h4>
+                                </div>
+
+                            </div>
+
+                            {/* Right Section */}
+                            <div className="flex flex-col items-start xl:items-end gap-4 ">
+
+                                <span className="w-fit bg-green-100 text-green-700 px-4 py-2 rounded-xl font-medium">
+                                    {account.status}
+                                </span>
+
+                                <button className="w-full xl:w-auto border border-blue-600 text-blue-600 px-6 py-3 rounded-xl hover:bg-blue-600 hover:text-white transition">
+                                    View Details →
+                                </button>
+
+                            </div>
+
                         </div>
 
-                        <div>
-                            <p className="text-gray-500">
-                                Inactive Accounts
-                            </p>
-
-                            <h2 className="text-3xl font-bold text-orange-500 mt-1">
-                                0
-                            </h2>
-                        </div>
-                    </div>
+                    </div>))}
 
                 </div>
             </div>
